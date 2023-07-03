56956ea72f38b7e1386bf8e760e5f4c5db0efc70
BilalJ0khio <jokhiobilal.11@gmail.com>
2023-07-03 15:41:27 +0500

    nav done1

--- app.js
+++ app.js
@@ -32,3 +32,40 @@ console.log(navEle);
 btnNavEle.addEventListener("click", function () {
   navEle.classList.toggle("open-nav");
 });
+
+//  for smooth scrolling maslo in safari incase
+
+// const allLinks = document.querySelectorAll("a:link");
+// console.log(allLinks);
+
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//   });
+// });
+
+// sticky navigation
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const nav = document.querySelector("body");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) nav.classList.add("sticky");
+    else nav.classList.remove("sticky");
+  },
+  {
+    // in the viewport
+    //  root means the viwport
+    root: null,
+    // this nav will observe when the hero section will move out of the view port
+    threshold: 0,
+    // unit must be in pixels means -70 pixels pehrya nav open thi wende
+    rootMargin: "-70px",
+  }
+);
+observer.observe(sectionHeroEl);
